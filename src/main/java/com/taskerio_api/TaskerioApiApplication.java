7a2c6d9e147644047e56e3ea6ce253f893910543
Eduardo Amaral <eduardo.lopes.amaral@outlook.com>
2023-02-26 18:10:47 -0300

    first commit

---
+++ src/main/java/com/taskerio_api/TaskerioApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskerio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskerioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taskerio_api/TaskerioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskerio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
